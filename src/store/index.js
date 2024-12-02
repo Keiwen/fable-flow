1945abd5fdb49372e452637dfa07bed4f2d79f09
Keiwen <keiwen.mk@gmail.com>
2024-12-02 21:18:27 +0100

    feat: FF-9 Draft code main directory setup page
    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,27 @@
 import { createStore } from 'vuex'
+import { checkPermission } from './permissionService'
 
 export default createStore({
   state: {
+    libraryHandle: null // default null
   },
   getters: {
+    hasLibraryDefined: (state) => state.libraryHandle !== null,
+    getLibraryHandle: (state) => state.libraryHandle,
+    getLibraryDirectory: (state) => state.libraryHandle ? state.libraryHandle.name : ''
   },
   mutations: {
+    setLibraryHandle (state, libraryHandle) {
+      state.libraryHandle = libraryHandle
+    }
   },
   actions: {
+    updateLibraryHandle ({ commit }, libraryHandle) {
+      commit('setLibraryHandle', libraryHandle)
+    },
+    async checkLibraryPermission ({ state }) {
+      return await checkPermission(state.libraryHandle)
+    }
   },
   modules: {
   }
