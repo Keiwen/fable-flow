0c28fa80ea8ba9f6e491336faac8925ec97f0c7d
Keiwen <keiwen.mk@gmail.com>
2024-12-05 23:43:12 +0100

    feat: FF-16 Configure build for docs folder
    use webhashhistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ChapterPlay from '../views/ChapterPlayView.vue'
 import BookSelection from '../views/BookSelectionView'
 
@@ -29,7 +29,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
