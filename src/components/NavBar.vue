03b0b75f8c8aa89ec3921af443bb07fdd1148ab1
Keiwen <keiwen.mk@gmail.com>
2024-12-10 19:04:34 +0100

    feat: FF-13 Create menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,12 +4,12 @@
 <template>
   <nav>
     <div class="nav-item">
-      <router-link to="/book">Book</router-link>
+      <router-link to="/book" aria-label="Navigate to book selection">Book</router-link>
     </div>
-    <div class="nav-item">
+    <div class="nav-item" aria-label="Navigate to chapter page">
       <router-link to="/">Chapter</router-link>
     </div>
-    <div class="nav-item">
+    <div class="nav-item" aria-label="Navigate to settings">
       <router-link to="/settings">Settings</router-link>
     </div>
   </nav>
