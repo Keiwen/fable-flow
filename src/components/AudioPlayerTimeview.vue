31e7e6414ada23102f9ab2e539e22597a826413f
Keiwen <keiwen.mk@gmail.com>
2024-12-11 22:06:20 +0100

    feat: FF-20 Media player design
    timeview component

---
+++ src/components/AudioPlayerTimeview.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+defineProps({
+  duration: { type: Number, default: 0 },
+  currentTime: { type: Number, default: 0 }
+})
+
+const getDisplayTime = (timeInSecond) => {
+  return store.getters.getDisplayTime(timeInSecond)
+}
+
+</script>
+
+<template>
+  <div class="custom-player-time">
+    <span class="custom-player-current-time">{{ getDisplayTime(currentTime) }}</span>
+    /
+    <span class="custom-player-duration">{{ getDisplayTime(duration) }}</span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
