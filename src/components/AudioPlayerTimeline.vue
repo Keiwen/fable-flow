df3d1c391a05e1a70c0f07f9c06bd6eac22b8732
Keiwen <keiwen.mk@gmail.com>
2024-12-11 22:06:37 +0100

    feat: FF-20 Media player design
    timeline component

---
+++ src/components/AudioPlayerTimeline.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  percentProgress: { type: Number, required: true }
+})
+const emit = defineEmits(['changeProgress'])
+
+// computed
+const cssVars = computed(() => {
+  return {
+    '--custom-player-percent-progress': props.percentProgress + '%'
+  }
+})
+
+// methods
+const changeTime = (e) => {
+  emit('changeProgress', e.srcElement.value)
+}
+
+</script>
+
+<template>
+  <div class="custom-player-timeline" :style="cssVars">
+    <input type="range" max="100" :value="percentProgress" @change="changeTime" aria-label="Player timeline">
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
